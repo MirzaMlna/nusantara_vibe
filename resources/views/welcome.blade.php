7da12e22c0eab9bfb1a265f03e176e1c81c33550
MirzaMlna <mirzamaulana713@gmail.com>
2024-11-21 14:45:19 +0800

    Auth Setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel + Bootstrap</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center text-primary">Welcome to Laravel with Bootstrap</h1>
+        <button class="btn btn-success">Click Me</button>
+    </div>
+</body>
+
+</html>
